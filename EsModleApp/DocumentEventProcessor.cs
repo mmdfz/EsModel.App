d10a5f6c175d41684ab1a4c021755b9e7be153be
Bin1991CN <50834420+Bin1991CN@users.noreply.github.com>
2024-12-29 19:51:45 +0800

    增加测试需求

---
+++ EsModleApp/DocumentEventProcessor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+
+namespace EsModleApp
+{
+    public abstract class DocumentEventHandler
+    {
+        protected SldWorks SwApp;
+        protected ModelDoc2 Document;
+
+        public DocumentEventHandler(ModelDoc2 doc, SldWorks app)
+        {
+            Document = doc;
+            SwApp = app;
+
+            AttachEventHandlers();
+        }
+
+        public virtual void AttachEventHandlers()
+        {
+            Document.DestroyNotify += OnDestroy;
+            Document.SaveNotify += OnSave;
+        }
+
+        public virtual void DetachEventHandlers()
+        {
+            Document.DestroyNotify -= OnDestroy;
+            Document.SaveNotify -= OnSave;
+        }
+
+        // 文档被销毁时的处理
+        protected virtual int OnDestroy()
+        {
+            DetachEventHandlers();
+            return 0; // 返回 0 表示处理成功
+        }
+
+        // 文档保存时的处理
+        protected virtual int OnSave()
+        {
+            SwApp.SendMsgToUser("Document is being saved.");
+            return 0; // 返回 0 表示处理成功
+        }
+    }
+}
---
+++ EsModleApp/SolidWorksAddin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using System.Collections;
+using System.Reflection;
+using SolidWorks.Interop.swpublished;
+using System.Diagnostics;
+using System.Windows.Forms;
+using SolidWorksTools;
+using SolidWorksTools.File;
+
+
+namespace EsModleApp
+{
+   
+}
