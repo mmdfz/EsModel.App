94dd1622e164b72d9531d12180bb81b819f886a5
mmdfz <154041449+mmdfz@users.noreply.github.com>
2024-12-30 17:02:32 +0800

    Update SolidWorksAddin.cs

--- EsModleApp/SolidWorksAddin.cs
+++ EsModleApp/SolidWorksAddin.cs
@@ -1,21 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Runtime.InteropServices;
+using System;
+using System.Windows.Forms;
 using SolidWorks.Interop.sldworks;
 using SolidWorks.Interop.swconst;
-using System.Collections;
-using System.Reflection;
 using SolidWorks.Interop.swpublished;
-using System.Diagnostics;
-using System.Windows.Forms;
-using SolidWorksTools;
-using SolidWorksTools.File;
 
-
-namespace EsModleApp
+namespace SolidWorksAddin
 {
-   
+    public class SolidWorksAddin : ISwAddin
+    {
+        private SldWorks _swApp;
+        private int _addInCookie;
+
+        #region ConnectToSW
+
+        public bool ConnectToSW(object swAppObj, int SessionCookie)
+        {
+            _swApp = (SldWorks)swAppObj;
+            _addInCookie = SessionCookie;
+
+            _swApp.SetAddinCallbackInfo(0, this, _addInCookie);
+
+            BuildMenu();
+
+            return true;
+        }
+
+        public bool DisconnectFromSW()
+        {
+            DestroyMenu();
+            return true;
+        }
+
+        private void BuildMenu()
+        {
+            int docType = (int)swDocumentTypes_e.swDocNONE;
+            _swApp.AddMenu(docType, "至易测试", 1); 
+            _swApp.AddMenuItem4(docType, _addInCookie, "打开至易测试窗体@至易测试", 1, "OpenTestForm", "3", "打开至易测试窗体", "");
+        }
+
+        private void DestroyMenu()
+        {
+            int docType = (int)swDocumentTypes_e.swDocNONE;
+            _swApp.RemoveMenu(docType, "至易测试", "OpenTestForm");
+        }
+
+        #endregion
+
+        #region Add-in Implementation
+
+        public void OpenTestForm()
+        {
+            IntPtr swMainWindowHandle = (IntPtr)_swApp.GetProcessID();
+
+            var swFrame = new swFrame(swMainWindowHandle);
+            var form = new TestForm(_swApp);
+            form.StartPosition = FormStartPosition.CenterParent;
+            form.ShowDialog(swFrame);  
+        }
+
+        #endregion
+    }
+    public class swFrame : IWin32Window
+    {
+        public IntPtr Handle { get; set; }
+
+        public swFrame(IntPtr handle)
+        {
+            Handle = handle;
+        }
+    }
 }
